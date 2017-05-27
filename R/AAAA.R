0644bccdcfae273debb2c32d25861462676e6446
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-05-27 16:07:43 -0300

    importar funções de outros pacotes

--- R/AAAA.R
+++ R/AAAA.R
@@ -1,3 +1,4 @@
 # Import functions from default packages other than `base` #####################
 # Source: http://stackoverflow.com/a/31314870/3365410
 #' @importFrom utils setTxtProgressBar txtProgressBar
+#' @importFrom stats na.omit
