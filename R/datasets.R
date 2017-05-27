32b2b8f079fe45578e782040b8392a36b924085e
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-05-27 16:07:58 -0300

    descarregar informações sobre os datasets

---
+++ R/datasets.R
@@ -0,0 +1,57 @@
+#' Get soil datasets
+#'
+#' Download dataset-specific data contained in the Brazilian Soil Iron Data Repository (Fe-BR).
+#'
+#' @param progress Show progress bar?
+#'
+#' @return A list with all of the data of the soil datasets contained in Fe-BR.
+#'
+#' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
+#' @seealso \url{http://www.ufsm.br/febr}
+#' @export
+#' @examples
+#' \dontrun{
+#' res <- datasets()
+#' str(res)
+#' }
+###############################################################################################################
+datasets <-
+  function (progress = TRUE) {
+
+    # Verificar consistência dos parâmetros
+    if (!is.logical(progress)) {
+      stop (paste("unknown value '", progress, "' passed to parameter progress", sep = ""))
+    }
+
+    # Descarregar chaves de identificação das planilhas do repositório
+    sheets_keys <- googlesheets::gs_key("18yP9Hpp8oMdbGsf6cVu4vkDv-Dj-j5gjEFgEXN-5H-Q", verbose = FALSE)
+    sheets_keys <- suppressMessages(
+      googlesheets::gs_read(sheets_keys, verbose = FALSE)
+    )
+
+    # Definir opções de local
+    locale <- readr::locale(decimal_mark = ",")
+
+    # Descarregar planilhas com camadas
+    if (progress) {
+      pb <- utils::txtProgressBar(min = 0, max = length(sheets_keys$dataset), style = 3)
+    }
+    obs <- list()
+    for (i in 1:length(sheets_keys$dataset)) {
+      tmp <- googlesheets::gs_key(sheets_keys$dataset[i], verbose = FALSE)
+      tmp <- suppressMessages(
+        googlesheets::gs_read_csv(tmp, na = c("NA", "-", ""), locale = locale, verbose = FALSE)
+      )
+
+      # Observações processadas
+      obs[[i]] <- tmp
+      if (progress) {
+        utils::setTxtProgressBar(pb, i)
+      }
+    }
+    if (progress) {
+      close(pb)
+    }
+
+    return (obs)
+  }
