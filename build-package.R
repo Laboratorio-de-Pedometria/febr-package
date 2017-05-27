72b39d447edf94f3b44895c0d7fce8d2479157f7
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-05-27 14:46:17 -0300

    atualização de informações básicas do pacote

---
+++ build-package.R
@@ -0,0 +1,17 @@
+# Build package
+
+# Dependencies
+update(devtools::package_deps())
+
+# turn on/off development mode
+devtools::dev_mode()
+
+# check examples and documentation
+devtools::check_man()
+devtools::run_examples(run = FALSE)
+
+# check the package for Linux and Windows
+devtools::check(document = TRUE, check_version = TRUE, force_suggests = TRUE, args = "--use-valgrind")
+
+# turn on/off development mode
+devtools::dev_mode()
