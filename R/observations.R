e7a8fbbac65cacb16d8993411ada6b6a65ce2714
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-08-02 11:26:58 -0300

    added option to transform coordinates

--- R/observations.R
+++ R/observations.R
@@ -12,6 +12,11 @@
 #' @param missing.coords What should be done with soil observations missing spatial coordinates? Options are
 #' \code{"drop"} (default) and \code{"keep"}.
 #'
+#' @param target.crs EPSG code defining the target coordinate reference system to which spatial coordinates
+#' should be transformed. Used only with \code{stack.obs = TRUE}. Defaults to \code{target.crs = "EPSG:4674"},
+#' i.e. SIRGAS 2000, the standard CRS for Brazil -- see more at \url{http://spatialreference.org/ref/epsg/}.
+#' If set to \code{target.crs = NULL} then the native spatial coordinates are returned.
+#'
 #' @param progress Show download progress bar?
 #'
 #' @details Standard columns and their content are as follows:
@@ -50,24 +55,45 @@
 #' }
 ###############################################################################################################
 observations <-
-  function (which.cols = "standard", stack.obs = TRUE, missing.coords = "drop", progress = TRUE) {
+  function (which.cols = "standard", stack.obs = TRUE, missing.coords = "drop", target.crs = "EPSG:4674",
+            progress = TRUE) {
 
     # Verificar consistência dos parâmetros
     if(!which.cols %in% c("standard", "all")) {
-      stop (paste("unknown value '", which.cols, "' passed to parameter which.cols", sep = ""))
+      stop (paste("Unknown value '", which.cols, "' passed to parameter which.cols", sep = ""))
     }
     if (!is.logical(stack.obs)) {
-      stop (paste("unknown value '", stack.obs, "' passed to parameter stack.obs", sep = ""))
+      stop (paste("Unknown value '", stack.obs, "' passed to parameter stack.obs", sep = ""))
     }
     if (which.cols == "all" && stack.obs == TRUE) {
       message("stack.obs can only be used with standard columns... switching to FALSE")
       stack.obs <- FALSE
     }
-    if(!missing.coords %in% c("drop", "keep")) {
-      stop (paste("unknown value '", missing.coords, "' passed to parameter missing.coords", sep = ""))
+    if (!missing.coords %in% c("drop", "keep")) {
+      stop (paste("Unknown value '", missing.coords, "' passed to parameter missing.coords", sep = ""))
+    }
+    if (!is.null(target.crs) && stack.obs == FALSE || which.cols == "all") {
+      message(paste(
+        "Coordinate transformation only works for stacked observations with standard columns\n",
+        "Setting target CRS to NULL...", sep = "")
+        )
+      target.crs <- NULL
+    }
+    crs_list <- paste("EPSG:", c(
+      # Córrego Alegre
+      4225, 22521, 22522, 22523, 22524, 22525,
+      # SAD69
+      4618 , 29168, 29188, 29169, 29189, 29170, 29190, 29191, 29192, 29193, 29194, 29195,
+      # WGS 84
+      4326, 32618, 32718, 32619, 32719, 32620, 32720, 32721, 32722, 32723, 32724, 32725,
+      # SIRGAS 2000
+      4674, 31972, 31978, 31973, 31979, 31974, 31980, 31981, 31982, 31983, 31984, 31985
+    ), sep = "")
+    if (!toupper(target.crs) %in% crs_list) {
+      stop (paste("Unknown value '", target.crs, "' passed to parameter target.crs", sep = ""))
     }
     if (!is.logical(progress)) {
-      stop (paste("unknown value '", progress, "' passed to parameter progress", sep = ""))
+      stop (paste("Unknown value '", progress, "' passed to parameter progress", sep = ""))
     }
 
     # Descarregar chaves de identificação das planilhas do repositório
@@ -130,9 +156,40 @@ observations <-
       close(pb)
     }
 
-    # Se necessário, empilhar tabelas e colocar colunas na ordem padrão
+    # Se necessário, empilhar tabelas
     if (stack.obs) {
-      obs <- suppressWarnings(dplyr::bind_rows(obs))[, target_cols]
+      obs <- suppressWarnings(dplyr::bind_rows(obs))
+
+      # Transformar SRC
+      if (!is.null(target.crs)) {
+        # Até que se prove o contrário, sempre haverá diversos SRC. Contudo, para evitar surpresas, é melhor
+        # inserir desde já o código para o caso de um único SRC.
+        if (nlevels(as.factor(obs$coord_sistema)) > 1) {
+          obs <- split(obs, as.factor(obs$coord_sistema))
+          if (toupper(target.crs) %in% names(obs)) {
+            j <- which(!names(obs) %in% toupper(target.crs))
+          } else {
+            j <- 1:length(obs)
+          }
+          obs <- lapply(obs[j], function (x) {
+            sp::coordinates(x) <- c("coord_x", "coord_y")
+            sp::proj4string(x) <- sp::CRS(paste("+init=", tolower(x$coord_sistema[1]), sep = ""))
+            x <- sp::spTransform(x, sp::CRS(paste("+init=", tolower(target.crs), sep = "")))
+            as.data.frame(x)
+          })
+          obs <- suppressWarnings(dplyr::bind_rows(obs))
+          obs$coord_sistema <- toupper(target.crs)
+        } else if (unique(obs$coord_sistema) != toupper(target.crs)) {
+          sp::coordinates(obs) <- c("coord_x", "coord_y")
+          sp::proj4string(obs) <- sp::CRS(paste("+init=", tolower(obs$coord_sistema[1]), sep = ""))
+          obs <- sp::spTransform(obs, sp::CRS(paste("+init=", tolower(target.crs), sep = "")))
+          obs <- as.data.frame(obs)
+          obs$coord_sistema <- toupper(target.crs)
+        }
+      }
+
+      # Colocar colunas na ordem padrão
+      obs <- obs[, target_cols]
     }
 
     return (obs)
