ec259ed36c9b430ce961c80b728e4e40d83bdafe
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-08-03 16:31:07 -0300

    redefinition of parameters; documentation

--- R/standards.R
+++ R/standards.R
@@ -1,9 +1,41 @@
+#' Standards
+#'
+#' Standards for soil variables in the Brazilian Soil Iron Data Repository (Fe-BR)  -- 
+#' \url{http://www.ufsm.br/febr}.
+#'
+#' @param soil.var Identification code of the soil variable. The only currently available option is 
+#' \code{soil.var = "fe"}.
+#' 
+#' @param extraction.method Identification code of the extraction method. See \sQuote{Details}.
+#' 
+#' @details
+#' Standards for soil variables are defined according to the third edition of the Manual of Methods of Soil
+#' Analysis published by Embrapa Soils in 2011. They include the measurement units and number of digital 
+#' places that should be used with a soil variable as defined by the extraction method.
+#' 
+#' @return A data.frame.
+#'
+#' @references
+#' Donagemma, G. K., Campos, D. V. B., Calderano, S. B., Teixeira, W. G., Viana, J. H. M. \emph{Manual de 
+#' Métodos de Análise de Solo}. Rio de Janeiro: Embrapa Solos, p. 230, 2011.
+#' 
+#' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
+#' @seealso \url{http://www.ufsm.br/febr}
+#' @export
+#' @examples
+#' standards()
+#' 
 # Definir unidade e número de casas decimais para cada tipo de dado de ferro ##################################
 standards <-
-  function (x = "fe", code) {
+  function (soil.var = "fe", extraction.method) {
 
+    # Verificar consistência dos parâmetros
+    if(!soil.var %in% c("fe")) {
+      stop (paste("Unknown value '", soil.var, "' passed to parameter soil.var", sep = ""))
+    }
+    
     switch(
-      x,
+      soil.var,
       "fe" = {
         # Segundo a terceira edição do Manual de Métodos de Análise de Solo (Donagemma et al. 2011), as
         # seguintes convenções devem ser aplicadas:
@@ -30,9 +62,10 @@ standards <-
       }
     )
 
-    if (!missing(code)) {
-      res <- res[res$code %in% code, ]
+    if (!missing(extraction.method)) {
+      res <- res[res$code %in% extraction.method, ]
     }
 
     return (res)
   }
+
