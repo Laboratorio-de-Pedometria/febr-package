c53684eda26b88f74172bb7e01c883c4861b6fb7
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-08-07 15:34:47 -0300

    helper function to select datasets

--- R/febrHelper.R
+++ R/febrHelper.R
@@ -15,4 +15,26 @@
       )
     )
   }
-
+# Which datasets should be downloaded? ----
+.getDataset <-
+  function (sheets_keys, dataset) {
+    
+    if ("all" %in% dataset) {
+      # Some datasets are not ready for download
+      sheets_keys <- sheets_keys[!is.na(sheets_keys$camada), ]
+      
+    } else {
+      idx_out <- which(!dataset %in% sheets_keys$ctb)
+      if (length(idx_out) >= 1) {
+        stop (paste("Unknown value '", dataset[idx_out], "' passed to parameter dataset", sep = ""))
+      }
+      sheets_keys <- sheets_keys[sheets_keys$ctb %in% dataset, ]
+      
+      # Some datasets might not be ready for download
+      idx_na <- which(is.na(sheets_keys$camada))
+      if (length(idx_na) >= 1) {
+        stop (paste("Cannot download dataset '", dataset[idx_na], "'. See https://goo.gl/tVC8dH", sep = ""))
+      }
+    }
+    return (sheets_keys)
+  }
