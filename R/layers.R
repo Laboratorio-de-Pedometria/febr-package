8315c8e070a2105c83d3c691a66e3ead80ad6e06
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-08-07 13:40:09 -0300

    several improvements for data standardization

--- R/layers.R
+++ R/layers.R
@@ -2,7 +2,11 @@
 #'
 #' Download soil layer-specific data contained in the Brazilian Soil Iron Data Repository (Fe-BR)  --
 #' \url{http://www.ufsm.br/febr}.
-#'
+#' 
+#' @param dataset Character vector with the identification code of the dataset -- or datasets -- for which 
+#' soil layer-specific data should be downloaded. Defaults to \code{dataset = "all"}, that is, download data
+#' from all existing datasets.
+#' 
 #' @param which.cols Which columns should be returned? Options are \code{"standard"} (default) and
 #' \code{"all"}.
 #'
@@ -18,7 +22,8 @@
 #'
 #' @param progress Show progress bar?
 #'
-#' @details Standard columns and their content are as follows:
+#' @details
+#' Standard columns and their content are as follows:
 #' \itemize{
 #' \item \code{dataset_id}. Identification code of the datasets in Fe-BR to which soil observations belong.
 #' \item \code{observacao_id}. Identification code of soil observations in Fe-BR.
@@ -30,20 +35,25 @@
 #' \item \code{fe_xxx_yyy}. Soil iron content data, with \code{xxx} being a given extraction method and
 #' \code{yyy} being a given measurement method.
 #' }
-#' @return A list or data.frame with some or all of the data of soil layers contained in Fe-BR.
+#' @return A list or data.frame with soil layer-specific data.
 #'
 #' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
 #' @seealso \url{http://www.ufsm.br/febr}, \code{\link[febr]{standards}}, \code{\link[febr]{conversion}}
 #' @export
 #' @examples
 #' \dontrun{
-#' res <- layers(which.cols = "standard", stack.layers = TRUE, missing.data = "drop", progress = TRUE)
+# res <- layers(dataset = paste("ctb000", 4:10, sep = ""), progress = FALSE)
 #' str(res)
 #' }
+###############################################################################################################
 layers <-
-  function (which.cols = "standard", stack.layers = TRUE, missing.data = "drop", harmonization = 2,
-            progress = TRUE) {
-
+  function (dataset = "all", which.cols = "standard", soil.vars = "fe",
+            stack.layers = TRUE, missing.data = "drop",
+            standardization = list(
+              plus.sign = "add", plus.depth = 2.5, transition = "smooth", smoothing.fun = "mean"),
+            harmonization = 1, 
+            progress = TRUE, verbose = TRUE) {
+     
     # Verificar consistência dos parâmetros
     if(!which.cols %in% c("standard", "all")) {
       stop (paste("Unknown value '", which.cols, "' passed to parameter which.cols", sep = ""))
@@ -65,18 +75,41 @@ layers <-
       stop (paste("Unknown value '", progress, "' passed to parameter progress", sep = ""))
     }
 
-    # Descarregar chaves de identificação das planilhas do repositório
-    sheets_keys <- googlesheets::gs_key("18yP9Hpp8oMdbGsf6cVu4vkDv-Dj-j5gjEFgEXN-5H-Q", verbose = FALSE)
-    sheets_keys <- suppressMessages(googlesheets::gs_read(sheets_keys, verbose = FALSE))
-
-    # Definir opções de local
-    locale <- readr::locale(decimal_mark = ",")
+    # Options
+    opts <- .opt()
+    
+    # Descarregar chaves de identificação das planilhas
+    sheets_keys <- 
+      googlesheets::gs_key("18yP9Hpp8oMdbGsf6cVu4vkDv-Dj-j5gjEFgEXN-5H-Q", verbose = opts$gs$verbose)
+    sheets_keys <- 
+      suppressMessages(googlesheets::gs_read(sheets_keys, verbose = opts$gs$verbose, na = opts$gs$na))
+    
+    # Which datasets should be downloaded?
+    if ("all" %in% dataset) {
+      # Some datasets are not ready for download
+      sheets_keys <- sheets_keys[!is.na(sheets_keys$camada), ]
+      
+    } else {
+      idx_out <- which(!dataset %in% sheets_keys$ctb)
+      if (length(idx_out) >= 1) {
+        stop (paste("Unknown value '", dataset[idx_out], "' passed to parameter dataset", sep = ""))
+      }
+      sheets_keys <- sheets_keys[sheets_keys$ctb %in% dataset, ]
+      
+      # Some datasets might not be ready for download
+      idx_na <- which(is.na(sheets_keys$camada))
+      if (length(idx_na) >= 1) {
+        stop (paste("Cannot download dataset '", dataset[idx_na], "'. See https://goo.gl/tVC8dH", sep = ""))
+      }
+    }
 
     # Definir as colunas padrão
     if (which.cols == "standard") {
-      target_cols <-
-        c("observacao_id", "camada_numero", "camada_nome", "amostra_codigo", "profund_sup", "profund_inf")
+      target_cols <- c(opts$layers$id.cols, opts$layers$depth.cols)
     }
+    
+    # Set IDs of soil variables
+    soil_vars <- paste(soil.vars, "_", sep = "")
 
     # Descarregar planilhas com camadas
     if (progress) {
@@ -84,90 +117,133 @@ layers <-
     }
     res <- list()
     for (i in 1:length(sheets_keys$camada)) {
-      tmp <- googlesheets::gs_key(sheets_keys$camada[i], verbose = FALSE)
-      unit <- suppressMessages(googlesheets::gs_read_csv(tmp, locale = locale, verbose = FALSE, n_max = 1))
+      if (verbose) {
+        message(paste("Downloading dataset ", sheets_keys$ctb[i], "...", sep = ""))
+      }
+      tmp <- googlesheets::gs_key(sheets_keys$camada[i], verbose = opts$gs$verbose)
+      unit <- suppressMessages(
+        googlesheets::gs_read_csv(tmp, locale = opts$gs$locale, verbose = opts$gs$verbose, n_max = 1))
+      unit <- as.data.frame(unit)
       # Solução rápida e suja enquanto as unidade de medida não são padronizadas
       unit[which(unit %in% "???")] <- "g/kg"
       unit[which(unit %in% "mg/dm3")] <- "mg/dm^3"
       tmp <- suppressMessages(
         googlesheets::gs_read_csv(
-          tmp, na = c("NA", "-", ""), locale = locale, verbose = FALSE, comment = "unidade")
+          tmp, na = opts$gs$na, locale = opts$gs$locale, verbose = opts$gs$verbose, comment = opts$gs$comment)
       )
-
-      # Identificar colunas potencialmente com dados de ferro
-      fe_cols <- colnames(tmp)[grep("^fe_", colnames(tmp))]
-
-      # Se houver, descartar colunas sem dados de ferro, atualizando a lista dos nomes das colunas com dados
-      # de ferro
-      idx_na <- which(apply(tmp[, fe_cols], 2, function (x) all(is.na(x))))
-      if (length(idx_na) >= 1) {
-        tmp <- tmp[, !colnames(tmp) %in% fe_cols[idx_na]]
-        fe_cols <- fe_cols[-idx_na]
-      }
-
-      # Definir as colunas a serem mantidas
-      if (which.cols == "standard") {
-        tmp <- tmp[, c(target_cols, fe_cols)]
-      }
-
-      # Se necessário, descartar camadas sem qualquer tipo de dado de ferro
-      if (missing.data == "drop") {
-        idx_keep <- is.na(tmp[, fe_cols])
-        idx_keep <- rowSums(idx_keep) < ncol(idx_keep)
-        tmp <- tmp[idx_keep, ]
-      }
-
-      # Verificar se, com a eliminação das camadas sem quaisquer dados de ferro, restou alguma camada
-      # Também é possível que o conjunto de dados não possua quaisquer dados de ferro
-      if (nrow(tmp) >= 1) {
-
-        # Identificar tipos de dado de ferro para padronização da unidade de medida e número de casas decimais
-        fe_type <- stringr::str_split_fixed(fe_cols, "_", n = 3)[, 2]
-        fe_stand <- lapply(fe_type, function (y) standards(soil.var = "fe", extraction.method = y))
-        fe_stand <- do.call(rbind, fe_stand)
-
-        # Se necessário, padronizar unidades de medida
-        idx_unit <- which(!unit[, fe_cols] %in% unique(standards(soil.var = "fe")$unit))
-        if (length(idx_unit) >= 1) {
-          conv_factor <- lapply(1:length(fe_type[idx_unit]), function (j) {
-            conversion(source = unlist(unit[, fe_cols[idx_unit]])[[j]], target = fe_stand$unit[idx_unit][j])
-          })
-          conv_factor <- do.call(rbind, conv_factor)
-          tmp[fe_cols[idx_unit]] <- t(t(tmp[fe_cols[idx_unit]]) * conv_factor$factor)
+      tmp <- as.data.frame(tmp)
+      
+      # CLEAN UP
+      # Identify the columns that should contain data on the chosen soil variables.
+      # Note that some of them might not contain any data at all, thus being filled-up with NAs. In this case, 
+      # the respective columns are discarded and, if any column with data remains, we continue with the data
+      # processing steps. In this case we also update the names of the soil variables.
+      soil_vars <- lapply(soil_vars, function (x) colnames(tmp)[grep(paste("^", x, sep = ""), colnames(tmp))])
+      soil_vars <- unlist(soil_vars)
+      idx_na <- which(apply(tmp[soil_vars], 2, function (x) all(is.na(x))))
+      
+      if (length(length(idx_na)) > length(soil_vars)) {
+        
+        # Update the names of soil variables
+        if (length(idx_na) >= 1) {
+          tmp <- tmp[, !colnames(tmp) %in% soil_vars[idx_na]]
+          soil_vars <- soil_vars[-idx_na]
         }
-
-        # Padronizar número de casas decimais
-        tmp[, fe_cols] <- sapply(1:length(fe_cols), function (j) {
-          round(tmp[, fe_cols[j]], digits = fe_stand$digits[j])
-        })
-        # Harmonizar dados de ferro
-        if (harmonization == 1) {
-          new_colnames <- matrix(stringr::str_split_fixed(colnames(tmp[, fe_cols]), "_", 3)[, 1:2], ncol = 2)
-          new_colnames <- apply(new_colnames, 1, paste, collapse = "_", sep = "")
-
-          # Nomes idênticos são gerados para variáveis definidas pelo mesmo extrator. Nesses casos mantém-se
-          # os nomes originais das respectivas colunas.
-          if (any(duplicated(new_colnames))) {
-            idx <- c(which(duplicated(new_colnames)), which(duplicated(new_colnames, fromLast = TRUE)))
-            new_colnames[idx] <- fe_cols[idx]
+        
+        # Definir as colunas a serem mantidas
+        if (which.cols == "standard") {
+          tmp <- tmp[, c(target_cols, soil_vars)]
+        }
+        
+        # STANDARDIZATION ----
+        # I. What to do with the plus sign?
+        if (standardization$plus.sign != "keep") {
+          tmp <- .setMaximumObservationDepth(
+            obj = tmp, id.col = target_cols[1], depth.cols = opts$layers$depth.cols,
+            plus.sign = standardization$plus.sign, plus.depth = standardization$plus.depth)
+        }
+        
+        if (standardization$transition != "keep") {
+          # II. What to do with wavy and irregular layer transitions?
+          tmp <- .solveIrregularLayerTransition(
+            obj = tmp, id.col = target_cols[1], depth.cols = opts$layers$depth.cols,
+            smoothing.fun = standardization$smoothing.fun)
+          
+          # III. What to do with broken layer transitions?
+          tmp <- .solveBrokenLayerTransition(
+            obj = tmp, id.cols = opts$layers$id.cols, depth.cols = opts$layers$depth.cols)
+        }
+        
+        # CLEAN UP ----
+        # Se necessário, descartar camadas sem qualquer tipo de dado de ferro
+        if (missing.data == "drop") {
+          idx_keep <- is.na(tmp[soil_vars])
+          idx_keep <- rowSums(idx_keep) < ncol(idx_keep)
+          tmp <- tmp[idx_keep, ]
+        }
+        
+        # Verificar se, com a eliminação das camadas sem quaisquer dados de ferro, restou alguma camada
+        # Também é possível que o conjunto de dados não possua quaisquer dados de ferro
+        if (nrow(tmp) >= 1) {
+          
+          # STANDARDIZATION ----
+          # Identificar tipos de dado de ferro para padronização da unidade de medida e número de casas decimais
+          fe_type <- stringr::str_split_fixed(soil_vars, "_", n = 3)[, 2]
+          fe_stand <- lapply(fe_type, function (y) standards(soil.var = "fe", extraction.method = y))
+          fe_stand <- do.call(rbind, fe_stand)
+          
+          # 1. Se necessário, padronizar unidades de medida
+          idx_unit <- which(!unit[, soil_vars] %in% unique(standards(soil.var = "fe")$unit))
+          if (length(idx_unit) >= 1) {
+            conv_factor <- lapply(1:length(fe_type[idx_unit]), function (j) {
+              conversion(source = unlist(unit[, soil_vars[idx_unit]])[[j]], target = fe_stand$unit[idx_unit][j])
+            })
+            conv_factor <- do.call(rbind, conv_factor)
+            tmp[soil_vars[idx_unit]] <- t(t(tmp[soil_vars[idx_unit]]) * conv_factor$factor)
           }
-          colnames(tmp)[colnames(tmp) %in% fe_cols] <- new_colnames
-        }
-        # Se as tabelas forem empilhadas, então 'observacao_id', 'profund_sup' e 'profund_inf' precisam estar
-        # no formato de caracter para evitar erros devido ao tipo de dado.
-        if (stack.layers) {
-          tmp$observacao_id <- as.character(tmp$observacao_id)
-          tmp$profund_sup <- as.character(tmp$profund_sup)
-          tmp$profund_inf <- as.character(tmp$profund_inf)
-        }
-        # Adicionar 'dataset_id' às camadas processadas.
-        res[[i]] <- cbind(dataset_id = as.character(sheets_keys$ctb[i]), tmp)
-
-        # Se as tabelas não são empilhadas, adicionar informação sobre unidade de medida
-        if (!stack.layers) {
-          a <- attributes(res[[i]])
-          a$units <- c(rep("unitless", 2), gsub("-", "unitless", as.character(unit)[-1]))
-          attributes(res[[i]]) <- a
+          # 2. Padronizar número de casas decimais
+          tmp[, soil_vars] <- sapply(1:length(soil_vars), function (j) {
+            round(tmp[, soil_vars[j]], digits = fe_stand$digits[j])
+          })
+          
+          # HARMONIZATION ----
+          # Harmonizar dados de ferro
+          if (harmonization == 1) {
+            new_colnames <- matrix(stringr::str_split_fixed(colnames(tmp[soil_vars]), "_", 3)[, 1:2], ncol = 2)
+            new_colnames <- apply(new_colnames, 1, paste, collapse = "_", sep = "")
+            
+            # Nomes idênticos são gerados para variáveis definidas pelo mesmo extrator. Nesses casos mantém-se
+            # os nomes originais das respectivas colunas.
+            if (any(duplicated(new_colnames))) {
+              idx <- c(which(duplicated(new_colnames)), which(duplicated(new_colnames, fromLast = TRUE)))
+              new_colnames[idx] <- soil_vars[idx]
+            }
+            colnames(tmp)[colnames(tmp) %in% soil_vars] <- new_colnames
+          }
+          
+          # STACKING ----
+          # If tables are to be stacked, then id.cols must be of type character
+          if (stack.layers) {
+            tmp[opts$layers$id.cols] <- sapply(tmp[opts$layers$id.cols], as.character)
+            
+            # If tables are to be stacked and depth data has not been standardized, then depth.cols must be 
+            # of type character. Otherwise depth.cols must be of type numeric.
+            if (standardization$plus.sign == "keep" || standardization$transition == "keep") {
+              tmp[opts$layers$depth.cols] <- sapply(tmp[opts$layers$depth.cols], as.character)
+            } else {
+              tmp[opts$layers$depth.cols] <- sapply(tmp[opts$layers$depth.cols], as.numeric)
+            }
+          }
+          
+          # Adicionar 'dataset_id' às camadas processadas.
+          res[[i]] <- cbind(dataset_id = as.character(sheets_keys$ctb[i]), tmp)
+          
+          # Se as tabelas não são empilhadas, adicionar informação sobre unidade de medida
+          if (!stack.layers) {
+            a <- attributes(res[[i]])
+            a$units <- c(rep("unitless", 2), gsub("-", "unitless", as.character(unit)[-1]))
+            attributes(res[[i]]) <- a
+          }
         }
       }
       if (progress) {
@@ -180,8 +256,8 @@ layers <-
     # Se necessário, empilhar tabelas, adicionando informações sobre as unidades de medida
     if (stack.layers) {
       res <- suppressWarnings(dplyr::bind_rows(res))
-      fe_cols <- colnames(res)[grep("^fe_", colnames(res))]
-      fe_type <- stringr::str_split_fixed(fe_cols, "_", n = 3)[, 2]
+      soil_vars <- colnames(res)[grep("^fe_", colnames(res))]
+      fe_type <- stringr::str_split_fixed(soil_vars, "_", n = 3)[, 2]
       fe_stand <- sapply(fe_type, function (y) standards(soil.var = "fe", extraction.method = y)$unit)
       a <- attributes(res)
       a$units <- c(rep("unitless", 5), rep("cm", 2), as.character(fe_stand))
--- build-package.R
+++ build-package.R
@@ -8,7 +8,7 @@ devtools::dev_mode()
 
 # check examples and documentation
 devtools::check_man()
-devtools::run_examples(run = FALSE)
+devtools::run_examples()
 
 # check the package for Linux and Windows
 devtools::check(document = TRUE, check_version = TRUE, force_suggests = TRUE, args = "--use-valgrind")
