7116e48ec9baf001e55bdbbadd9e1b7c7fe3d144
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-12-04 00:28:28 -0200

    build package

--- build-package.R
+++ build-package.R
@@ -4,7 +4,7 @@
 update(devtools::package_deps())
 
 # turn on/off development mode
-devtools::dev_mode()
+# devtools::dev_mode()
 
 # check examples and documentation
 devtools::check_man()
@@ -13,5 +13,9 @@ devtools::run_examples()
 # check the package for Linux and Windows
 devtools::check(document = TRUE, check_version = TRUE, force_suggests = TRUE, args = "--use-valgrind")
 
+devtools::build_win()
+
+devtools::build()
+
 # turn on/off development mode
-devtools::dev_mode()
+# devtools::dev_mode()
