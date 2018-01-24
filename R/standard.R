3cc3af64f43f7c564226213e81f223865d12bbe0
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-01-24 15:35:41 -0200

    nova descrição padronizada da função; avaliação de argumentos e mensagens de erro

--- R/standard.R
+++ R/standard.R
@@ -1,69 +1,115 @@
 #' Get *standard* table
 #'
-#' Definitions of standards for soil variables in the Free Brazilian Repository for Open Soil Data -- 
-#' \url{http://www.ufsm.br/febr}.
-#'
-#' @param soil.var Identification code of the soil variable. The only currently available option is 
-#' \code{soil.var = "fe"}.
+#' Download data from the *standard* ("padrao") table of the Free Brazilian Repository for Open Soil Data --
+#' ___febr___, \url{http://www.ufsm.br/febr}. This includes codes and names of variable, measurement units, 
+#' number of decimal places, type of data, and description of analytical methods. This is used to standardize 
+#' the data contained in a dataset when downloading it via \code{\link[febr]{layer}} or 
+#' \code{\link[febr]{observation}}.
 #' 
-#' @param extraction.method Identification code of the extraction method. See \sQuote{Details}.
+#' @param table (optional) Character string indicating a table, i.e. the *layer* table, `"camada"`, or the 
+#' *observation* table, `"observacao"`.
 #' 
-#' @details
-#' Standards for soil variables are defined according to the third edition of the Manual of Methods of Soil
-#' Analysis published by Embrapa Soils in 2011. They include the measurement units and number of digital 
-#' places that should be used with a soil variable as defined by the extraction method.
+#' @param variable (optional) Character vector indicating one or more variables. Accepts both specific 
+#' identification codes, e.g. `"ferro_oxalato_icpoes"` and `"carbono_dicromato_30min150_mohr"`, as well as 
+#' general identification codes, e.g. `"ferro"` and `"carbono"`.
 #' 
-#' @return A data.frame.
+#' @param unit (optional) Character vector indicating one or more measurement units. For example, `"g/kg"`, 
+#' `"g/cm^3"`, and `"cmolc/kg"`.
+#' 
+#' @param precision (optional) Integer vector indicating one or more number of decimal places.
+#' 
+#' @param expr (optional) Character string to be parsed and evaluated as a regular expression. For example, 
+#' `"campo_precisao > 0"`. Usage requires some knowledge of the structure of the *standard* table.
+#' 
+#' @return A data frame with standards for selected variable(s).
 #'
 #' @references
 #' Donagemma, G. K., Campos, D. V. B., Calderano, S. B., Teixeira, W. G. and Viana, J. H. M. (2011)
-#' \emph{Manual de Métodos de Análise de Solo}. Rio de Janeiro: Embrapa Solos.
+#' *Manual de Métodos de Análise de Solo*. Rio de Janeiro: Embrapa Solos.
 #' 
 #' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
-#' @seealso \url{http://www.ufsm.br/febr}
-# @export
+#' @seealso The *standard* table at \url{https://goo.gl/mwwZN9}
+#' @export
 #' @examples
-#' # All standards
-#' standards()
-#' 
-#' # Specific stantdard
-#' standards(extraction.method = "sulfurico")
-#' 
+#' \dontrun{
+#' a <- standard(variable = "ferro")
+#' }
 ###############################################################################################################
 standard <-
   function (table, variable, unit, precision, expr) {
-
-    # Descarregar tabela com padrões
+    
+    # ARGUMENTOS
+    ## table
+    if (!missing(table) && !table %in% c("observacao", "camada")) {
+      stop (glue::glue("unknown value '{table}' passed to argument 'table'"))
+    }
+    
+    ## variable
+    if (!missing(variable) && !is.character(variable)) {
+      stop (glue::glue("object of class '{class(variable)}' passed to argument 'variable'"))
+    }
+    
+    ## unit
+    if (!missing(unit) && !is.character(unit)) {
+      stop (glue::glue("object of class '{class(unit)}' passed to argument 'unit'"))
+    }
+    
+    ## precision
+    if (!missing(precision) && !pedometrics::isNumint(precision)) {
+      stop (glue::glue("object of class '{class(precision)}' passed to argument 'precision'"))
+    }
+    
+    ## expr
+    if (!missing(expr) && !is.character(expr)) {
+      stop (glue::glue("object of class '{class(expr)}' passed to argument 'expr'"))
+    }
+    
+    # DESCARREGAMENTO
+    ## Descarregar tabela com padrões
     std <- .getTable(x = "1Dalqi5JbW4fg9oNkXw5TykZTA39pR5GezapVeV0lJZI")
 
-    # Selecionar por tabela_id
+    # PROCESSAMENTO
+    ## Selecionar por tabela_id
     if (!missing(table)) {
       idx <- which(std$tabela_id == table)
       std <- std[idx, ]
     }
     
-    # Selecionar por campo_id
+    ## Selecionar por campo_id
     if (!missing(variable)) {
-      idx <- which(which(std$campo_id == variable))
+      is_start <- all(grepl(pattern = "_", x = variable))
+      if (!is_start) {
+        variable <- glue::glue("^{variable}_")
+        idx <- lapply(variable, function (pattern) grep(pattern = pattern, x = std$campo_id))
+        idx <- unlist(idx)
+      } else {
+        idx <- which(std$campo_id %in% variable) 
+      }
       std <- std[idx, ]
     }
     
-    # Selecionar por campo_unidade
+    ## Selecionar por campo_unidade
     if (!missing(unit)) {
-      idx <- which(std$campo_unidade == unit)
+      idx <- which(std$campo_unidade %in% unit)
       std <- std[idx, ]
     }
     
-    # Selecionar por campo_precisao
+    ## Selecionar por campo_precisao
     if (!missing(precision)) {
-      idx <- which(std$campo_precisao == precision)
+      idx <- which(std$campo_precisao %in% precision)
       std <- std[idx, ]
     }
     
-    # Selecionar usando expressão
+    ## Selecionar usando expressão
     if (!missing(expr)) {
       std <- dplyr::filter(std, eval(parse(text = expr)))
     }
     
+    # ERRO
+    if (nrow(std) == 0) {
+      stop ("function call did not return any results", call. = TRUE)
+    }
+    
+    # FINAL
     return (std)
   }
