ead6c9898f76acaa089b70a4c6b8bef13a007892
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-02-27 14:16:26 -0300

    função para criar objeto de classe SpatialPointsDataFrame

---
+++ R/febr2sp.R
@@ -0,0 +1,39 @@
+#' Create an object of class `SpatialPointsDataFrame`
+#' 
+#' Set spatial coordinates and projection attributes to create an object of class `SpatialPointsDataFrame` from
+#' the *observation* ("observacao") table of one or more standardized datasets contained in the Free Brazilian
+#' Repository for Open Soil Data -- ___febr___, \url{http://www.ufsm.br/febr}.
+#' 
+#' @param obj Object of class `data.frame` downloaded from ___febr___ using function
+#' \code{\link[febr]{observation}}.
+#' 
+#' @return An object of class `SpatialPointsDataFrame`
+#' 
+#' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
+#' @export
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#' observation(dataset = "ctb0003", variable = "taxon",
+#'             progress = FALSE, verbose = FALSE) %>% 
+#'   febr2sp() %>%
+#'   spplot(zcol = "taxon_sibcs_2009", auto.key = list(columns = 3), scales = list(draw = TRUE)) +
+#'   latticeExtra::layer(panel.grid(v = -1, h = -1))
+#' }
+###############################################################################################################
+febr2sp <-
+  function (obj) {
+    
+    # Verificar sistema de referência de coordenadas
+    crs <- unique(obj$coord_sistema)
+    n_crs <- length(crs)
+    if (n_crs == 1) {
+      sp::coordinates(obj) <- ~ coord_x + coord_y
+      sp::proj4string(obj = obj) <- sp::CRS(glue::glue("+init={tolower(crs)}"))
+      obj@data <- dplyr::select(obj@data, -coord_sistema)
+    } else {
+      stop ("spatial coordinates have not been standardized")
+    }
+    
+    return (obj)
+  }
