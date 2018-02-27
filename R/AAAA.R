a1179c47099bc9d33707df4715555bc2c1edb16a
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-02-27 15:10:59 -0300

    define variáveis globais

--- R/AAAA.R
+++ R/AAAA.R
@@ -1,3 +1,10 @@
+# Supress R CMD check note 'no visible binding for global variable ...' ########
+# Source: http://stackoverflow.com/a/12429344/3365410
+# These variables are generated within the functions created with 'autofun'
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    names = c("coord_sistema"))
+}
 # Import functions from default packages other than `base` #####################
 # Source: http://stackoverflow.com/a/31314870/3365410
 #' @importFrom utils setTxtProgressBar txtProgressBar
