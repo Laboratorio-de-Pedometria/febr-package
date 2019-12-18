c252f6c5918fe4252b94eafda07ee7ba1fb8dd8a
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-12-17 23:37:03 -0300

    mais testes com nova função para leitura dos dados

--- tmp_read.R
+++ tmp_read.R
@@ -1,10 +1,11 @@
 library(magrittr)
 # https://stackoverflow.com/questions/33713084/download-link-for-google-spreadsheets-csv-export-with-multiple-sheets
 
-key <- '1ewJgirY3h4c5X5DApGGfkb1z8XALKoBiSh8vKd5JZhA'
+key <- '1jLQqkDjIT_N86mmRRbaAClU21UBbcmXJI7MiA8zg_Ig'
 sheet_name <- 'camada'
-glue::glue('https://docs.google.com/spreadsheets/d/{key}/gviz/tq?tqx=out:csv&sheet={sheet_name}&headers=3') %>% 
-  read.table(header = TRUE, sep = ',', dec = ',', check.names = FALSE, stringsAsFactors = FALSE) %>% 
+headers <- 1
+glue::glue("https://docs.google.com/spreadsheets/d/{key}/gviz/tq?tqx=out:csv&sheet={sheet_name}&headers={headers}?tq=where%20observacao_id%20!%3D%20'%23metadado%3E'") %>% 
+  read.table(header = TRUE, sep = ',', dec = ',', check.names = FALSE, stringsAsFactors = FALSE, na.strings = c('NA', '-')) %>% 
   tibble::as_tibble() %>% 
   colnames()
 
