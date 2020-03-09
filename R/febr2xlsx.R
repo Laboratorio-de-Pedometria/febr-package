8a65e220d406ff7808558307ff7a0ed603d703e3
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-03-09 11:55:04 -0300

    update documentation -- deprecated function

--- R/febr2xlsx.R
+++ R/febr2xlsx.R
@@ -10,7 +10,7 @@
 #' @param row.names (optional) Logical value indicating whether the row names of `x` are to be written along 
 #' with `x` to the file.
 #' 
-#' @param ... (optional) Further arguments passed to \code{\link[xlsx]{write.xlsx}}.
+#' @param ... (optional) Further arguments passed to write function.
 #' 
 #' @note THIS FUNCTION IS DEPRECATED. PLEASE USE \code{\link[openxlsx]{write.xlsx}} INSTEAD.
 #' 
@@ -18,12 +18,12 @@
 #' @export
 #' @examples
 #' \donttest{
-#' dts <-
-#'   febr(dataset = "ctb0013",
-#'        variable = "all",
-#'        merge = TRUE,
-#'        progress = FALSE, verbose = FALSE)
-#' febr2xlsx(x = dts, file = tempfile(fileext = ".xlsx"))
+#' # dts <-
+#' #   febr(dataset = "ctb0013",
+#' #        variable = "all",
+#' #        merge = TRUE,
+#' #        progress = FALSE, verbose = FALSE)
+#' # febr2xlsx(x = dts, file = tempfile(fileext = ".xlsx"))
 #' }
 ###############################################################################################################
 febr2xlsx <-
