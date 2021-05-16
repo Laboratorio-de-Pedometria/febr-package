9fec514f988bbdd9658995f1a3b5e1b71a00472b
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2021-05-16 13:28:36 -0300

    arguments passed to data.table

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,12 @@
 #' Utilities
 #' 
+#' @param ... (optional) Arguments passed to \code{\link[data.table]{fread}}.
+#' 
 #' @author Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
 #' @export
-readIndex <- function() {
+readIndex <- function(...) {
   owncloud <- "https://cloud.utfpr.edu.br/index.php/s/Df6dhfzYJ1DDeso/download?path=%2F"
-  febr_index <- data.table::fread(input = paste0(owncloud, "&files=febr-indice.txt"), dec = ",")
+  febr_index <- data.table::fread(
+    input = paste0(owncloud, "&files=febr-indice.txt"), dec = ",", ...)
   return(febr_index)
 }
